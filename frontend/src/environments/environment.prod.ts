1486600562145611a3b8d1fd0b2855d215be4774
Pavan Kumar Mamidi <mamidipavankumar3@gmail.com>
2026-04-25 14:33:56 -0400

    updated the backend realtime URL

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://your-backend.onrender.com', // Replace with your Render backend URL
+  apiUrl: 'https://vivapetals.onrender.com',
   googleClientId: '446077785747-51ofd16mdj7i07dtfh38d3uufbu9jgck.apps.googleusercontent.com',
   facebookAppId: 'YOUR_FACEBOOK_APP_ID'
 };
